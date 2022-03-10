9d801fef123979d698b5a8a8681228c032ff8732
devansh-einfo <98091739+devansh-einfo@users.noreply.github.com>
2022-03-10 15:13:03 +0530

    Add files via upload

---
+++ wrfio2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(){
+	int fd, len;
+	int x;
+	char write_buf[60] = "Linux kernel";
+	char read_buf[60];
+	fd = open("linuxa3.txt", O_CREAT | O_RDWR, 0777);
+	len = write(fd, write_buf, 60);
+	printf("Return = %d", len);
+//Using Lseek function to reset offset to beginning	
+	lseek(fd, -55 , SEEK_END);
+
+	read(fd,read_buf,len);
+	printf("\nRead=%s", read_buf);
+
+	close(fd);
+}
